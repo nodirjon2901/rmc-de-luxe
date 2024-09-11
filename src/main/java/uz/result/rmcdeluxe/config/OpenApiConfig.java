62e5861af8b14a5936de2c24f9b679d7ad16ccff
nodirjon2901 <nodirjontojiahmedov2901@gmail.com>
2024-09-11 10:44:53 +0500

    Commit by Nodir. Change port

--- src/main/java/uz/result/rmcdeluxe/config/OpenApiConfig.java
+++ src/main/java/uz/result/rmcdeluxe/config/OpenApiConfig.java
@@ -20,9 +20,9 @@ public class OpenApiConfig
                         .version("1.0")
                         .description("Backend sila karoche \uD83D\uDCAA \uD83D\uDCAA\uD83C\uDFFF"))
                 .servers(List.of(
-                        new Server().url("http://127.0.0.1:8150").description("Localhost"),
-                        new Server().url("http://213.230.91.55:8150").description("Server with IP"),
-                        new Server().url("https://rmc-de-luxe.uz/api").description("Server with domain")
+                        new Server().url("http://127.0.0.1:8210").description("Localhost"),
+                        new Server().url("http://213.230.91.55:8210").description("Server with IP")
+//                        new Server().url("https://rmc-de-luxe.uz/api").description("Server with domain")
                 ));
     }
 }
